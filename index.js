a0a4a7bd529641841493c59e657f90c1b2d78f14
quangloc.nguyen <quangloc.nguyen>
2020-07-29 16:50:43 +0700

    post

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const courses = [{ id: 1, name: "Node js" }];
+app.use(express.json());
+const courses = [
+  { id: 1, name: "Node js", id: 2, name: "Angular", id: 3, name: "Swift" },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -16,6 +19,15 @@ app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((e) => e.id === parseInt(req.params.id));
   if (!course) res.status(404).send("The course not found");
   res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(courses);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT} ...`));
